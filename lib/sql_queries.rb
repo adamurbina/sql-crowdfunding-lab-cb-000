3c60bb03abc20d57e6fb63c0b0b9d6a426a89792
Adam Urbina <adamurbina@gmail.com>
2018-07-21 12:21:16 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -41,7 +41,10 @@ ORDER BY SUM(pledges.amount), users.name;"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-""
+"SELECT projects.category, SUM(pledges.amounts)
+FROM pledges
+INNER JOIN projects
+ON pledges.project_id = projects.id"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
