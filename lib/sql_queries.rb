99cc1a6e3fa4553c5bec0296754864687d657eb3
Adam Urbina <adamurbina@gmail.com>
2018-07-21 12:00:15 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -17,7 +17,7 @@ end
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
 "SELECT users.name, users.age, pledges.amount
 FROM pledges
-INNER JOIN uers
+INNER JOIN users
 ON user_id = users.id
 GROUP BY users.name;"
 end
